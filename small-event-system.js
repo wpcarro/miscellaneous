8161a87936db0e86b44933405dc734581998f5b4
William Carroll <wpcarro@users.noreply.github.com>
2016-07-25 23:23:58 -0400

    Updates code to use ES6 syntax

--- small-event-system.js
+++ small-event-system.js
@@ -1,27 +1,42 @@
-// A small, simple implementation of an event-system
-var mixEvents = function(obj) {
+// A small, simple implementation of an pub-sub event system
+// This example embraces the mix-in design pattern.
+
+/**
+ * @param {Object} obj The object being extended with the
+ *     mixins defined within the function body.
+ * @ return {Object} The same reference to the object that
+ *     was initially passed in with two additional
+ *     methods: trigger and on.
+ */
+var mixEvents = obj => {
+  /** @private {Object} */
   var events = {};
-
-  // trigger event-handlers
-  obj.trigger = function (event) {
-    var args = [].slice.call(arguments, 1)
-    if (!events[event]) {
-      return;
-    }
-    events[event].forEach(function(cb) {
-      cb.apply(this, args);
-    });
-  };
-
-  // register events
-  obj.on = function (event, callback) {
-    if (!events[event]) {
-      events[event] = [callback];
-      return obj;
-    }
-    events[event].push(callback);
-    return obj;
-  };
   
-  return obj;
+  /**
+   * @param {string} event Name of the event to trigger.
+   * @ return {(Array<*>|false)} Returns an array of the
+   *     values returned from each cb stored on the events
+   *     hash. If there is no event registered, then this
+   *     function returns false.
+   */
+  const trigger = event => 
+    !events[event]
+      ? false
+      : events[event].map(cb => cb());
+  
+  /**
+   * @param {string} event Name of the event being registered.
+   * @param {function} cb The callback function to fire when
+   *     the event is triggered.
+   */
+  const on = (event, cb) => 
+    !events[event]
+      ? events[event] = [cb]
+      : events[event].push(cb);
+  
+
+  return Object.assign(obj, {
+    trigger,
+    on
+  });
 };

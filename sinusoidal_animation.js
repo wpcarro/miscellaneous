137012a45fb04193c41406552c012d960280d5bd
William Carroll <wpcarro@users.noreply.github.com>
2016-08-08 10:35:28 -0400

    Creates self-contained animation file

---
+++ sinusoidal_animation.js
@@ -0,0 +1,40 @@
+(function main() {
+
+  const step_amt = 0.050;
+	
+  let target;
+  let throttle_amt_ms = 0;
+  let x = 0;
+
+  (function setup_document() {
+    const target_styles = {
+      border: '1px solid red',
+      color: 'red',
+      display: 'inline-block',
+      fontFamily: 'monospace',
+      height: '15px',
+      marginTop: '100px',
+      position: 'absolute',
+      textAlign: 'center',
+      width: '75px'
+    };
+
+    target = document.createElement('div');
+    target.innerText = 'hello';
+    Object.assign(target.style, target_styles);
+    document.body.appendChild(target);
+  }());
+  
+  const update_target = val => {
+    target.innerText = val;
+    target.style.top = `${val * -100}px`;
+  };
+
+  const update = () => {
+    update_target(Math.sin(x).toFixed(3));
+    x += step_amt;
+    window.setTimeout(() => window.requestAnimationFrame(update), throttle_amt_ms);
+  };
+  
+  window.requestAnimationFrame(update);
+}());

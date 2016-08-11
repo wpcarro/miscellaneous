81112c67e76eca7e35c21284357e28e7693627e5
William Carroll <wpcarro@users.noreply.github.com>
2016-08-11 10:28:44 -0400

    Creates base file for generator experiments

---
+++ generator_experiments.js
@@ -0,0 +1,22 @@
+/**
+ * This file hosts functions intended to stress-test my understanding of ES6
+ * generator functions. The goal is to discover an expressive coding style
+ * for asynchronous behavior.
+ */
+
+function run() {
+
+  function httpGet(g) {
+    fetch('https://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe')
+      .then(res => res.json())
+      .then(json => g.next(json));
+  }
+  
+  function* logApiCall() {
+    var json = yield httpGet(generator);
+    console.log(JSON.stringify(json, null, 2));
+  }
+
+  const generator = logApiCall();
+  generator.next();
+}

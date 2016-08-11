383340f7f2336565334342818ae7bb593e035eb6
William Carroll <wpcarro@users.noreply.github.com>
2016-08-11 11:15:59 -0400

    Updates code to ES6

--- char-frequency.js
+++ char-frequency.js
@@ -1,38 +1,29 @@
-// Function that takes as its input a string and returns an array of 
-// arrays as shown below sorted in descending order by frequency and 
-// then by ascending order by character.
+/**
+ * Function that takes as its input a string and returns an array of 
+ * arrays sorted first in descending order by frequency and 
+ * secondly in ascending order by character.
+ */
 
-// This solution illustrates a solution using Array built-ins
-// that are often used in functional programming environments
+function setValue(obj, prop, value) {
+  const clone = Object.assign({}, obj);
+  clone[prop] = value;
+  return clone;
+}
 
 function characterFrequency (string) {
-  var result = string
-    .split('')
-    .reduce(function(acc, next) {
-      if(acc.hasOwnProperty(next)) {
-        acc[next]++;
-      } else {
-        acc[next] = 1;
-      }
-      return acc;
-    }, {});
+  const freqTable = string.split('')
+    .reduce((freqTable, char) => 
+      setValue(freqTable, char, (freqTable[char] || 0) + 1), {});
   
-  var toArr = [];
-  var tuple;
-  for (var key in result) {
-    tuple = [];
-    tuple.push(key);
-    tuple.push(result[key]);
+  const toArr = [];
+  let tuple;
+  for (let char in freqTable) {
+    tuple = [char, freqTable[char]];
     toArr.push(tuple);
   }
   return toArr
-    .map(function(tuple) {
-      return tuple.concat(tuple[1] *  127 - tuple[0].charCodeAt());
-    })
-    .sort(function(a, b) {
-      return b[2] - a[2];
-    })
-    .map(function(tuple) {
-      return tuple.slice(0, 2);
-    });
+    .map(tuple =>
+      tuple.concat(tuple[1] *  127 - tuple[0].charCodeAt()))
+    .sort((a, b) => b[2] - a[2])
+    .map(tuple => tuple.slice(0, 2));
 }

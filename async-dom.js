e674b8b04645f3459dd606600be8ae7c87bb2345
William Carroll <wpcarro@users.noreply.github.com>
2016-10-28 00:48:43 -0400

    Create async-dom.js

---
+++ async-dom.js
@@ -0,0 +1,36 @@
+/**
+ * Retrieves a DOM element. If the element is not present, observes changes to
+ * the DOM if the element is not present.
+ * @param {string} selector CSS selector string.
+ * @return {Element} DOM element reference.
+ */
+const querySelectorAsync = selector => new Promise(resolve => {
+  const element = document.querySelector(selector)
+
+  if (element) {
+    return resolve(element)
+  }
+
+  const observer = new MutationObserver(mRecords => {
+    mRecords.forEach(mRecord => mRecord.addedNodes.forEach(node => {
+      let element
+
+      try {
+        element = node.parentElement.querySelector(selector)
+      } catch (err) {
+        element = null
+      }
+
+      if (element) {
+        resolve(element)
+        observer.disconnect()
+      }
+    }))
+  })
+
+  observer.observe(document.body, { childList: true })
+})
+
+export {
+  querySelectorAsync
+}
